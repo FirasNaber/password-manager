14f44a8f7f508bb55974f758c83a7cf9c6bedffb
Firas Naber <firasnaber@gmail.com>
2022-10-17 21:52:27 -0500

    Add search functionality and saved data file type to JSON

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
 import pyperclip as pc
 from random import choice, randint, shuffle
 from tkinter import Button, Canvas, END, Entry, Label, messagebox, PhotoImage, Tk
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -9,8 +11,10 @@ def generate():
     if len(password_entry.get()) > 0:
         password_entry.delete(first=0, last=END)
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -31,23 +35,59 @@ def generate():
     password_entry.insert(index=0, string=password)
     pc.copy(password)
 
+# ---------------------------- SEARCH WEBSITE ------------------------------ #
+
+
+def search():
+    website = website_entry.get().lower()
+
+    try:
+        with open(file="data.json") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="Data file not found.")
+    else:
+        if website in data.keys():
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=f"{website.title()} Login Information", message=f"Email: {email}"
+                                                                                      f"\nPassword: {password}")
+        elif website == "":
+            messagebox.showerror(title="Error", message="Please enter a website to search for.")
+        else:
+            messagebox.showerror(title="Error", message=f"No login information for {website} exists.")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
-def save():
+def add():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website.lower(): {
+            "email": email.lower(),
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
-        messagebox.showerror(title="Validation Error", message="Please fill all fields")
+        messagebox.showerror(title="Validation Error", message="Please fill all fields.")
     else:
-        with open(file="data.txt", mode="a") as file:
-            file.write(f"{website} | {email} | {password}\n")
+        try:
+            with open('data.json') as data_file:
+                data = json.load(data_file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = new_data
+        else:
+            data.update(new_data)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
-    website_entry.delete(first=0, last=END)
-    password_entry.delete(first=0, last=END)
+        with open('data.json', mode='w') as data_file:
+            json.dump(data, data_file, indent=4)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -70,8 +110,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0, sticky="W")
 
 # Input Boxes
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, sticky="EW")
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
@@ -80,9 +120,11 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
+search_button = Button(text="Search", command=search)
+search_button.grid(row=1, column=2, sticky="EW")
 generate_password_button = Button(text="Generate Password", command=generate)
-generate_password_button.grid(row=3, column=2)
-add_password_button = Button(text="Add", width=35, command=save)
+generate_password_button.grid(row=3, column=2, sticky="EW")
+add_password_button = Button(text="Add", width=35, command=add)
 add_password_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 window.mainloop()
